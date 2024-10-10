3e8359c7873130d1718d9fe9f8efa760e3d34856
M. Dguez. <mdomingriv1@educacion.navarra.es>
2024-10-10 19:18:57 +0200

    Add files via upload
    Archivo con errores básicos PEP8.

---
+++ PEP8_coregir.py
@@ -0,0 +1,12 @@
+# script.py
+import os, sys
+def MiFuncionSuma(A, B, C, imprime = True):
+    resultado=A+B+C
+    if imprime != False:
+        print(resultado)
+    return resultado
+a          = 4
+variable_b = 5
+var_c      = 10
+
+MiFuncionSuma(a, variable_b, var_c)
