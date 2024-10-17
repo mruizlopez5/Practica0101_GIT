6092a6372c759b9d4d064d2055d85173eef4b96a
aboshnasta <mruizlopez5@educacion.navarra.es>
2024-10-17 19:15:29 +0200

    Update PEP8_coregir.py
    corregidos 2 lineas antes y despues de la definicion, linea 4,5,11,12

--- PEP8_coregir.py
+++ PEP8_coregir.py
@@ -1,11 +1,15 @@
 # script.py
 import os
 import sys
+
+
 def MiFuncionSuma(A, B, C, imprime = True):
     resultado=A+B+C
     if imprime != False:
         print(resultado)
     return resultado
+
+
 a          = 4
 variable_b = 5
 var_c      = 10
